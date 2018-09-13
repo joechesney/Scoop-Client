d0cd1590470da34545287492b6ed584f3ff7899f
joechesney <33613119+joechesney@users.noreply.github.com>
2018-09-12 21:02:14 -0500

    ignored all the production stuff, and added media queries for the navbar

---
+++ src/Components/ShowMore.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+const ShowMoreButton = props => {
+
+  return (
+    <button className="showMoreButton"  onClick={props.showMoreProducts}>Show More</button>
+  )
+}
+
+export default ShowMoreButton;
